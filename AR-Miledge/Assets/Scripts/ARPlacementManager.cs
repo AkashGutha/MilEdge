f27513c0bbcd94953ef80dd59d3cc7105b62ab83
AkashGutha <akash.gutha@outlook.com>
2017-11-15 23:22:00 +0530

    shifted arkit to plugins folder

---
+++ AR-Miledge/Assets/Scripts/ARPlacementManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARPlacementManager : MonoBehaviour {
+
+	// public variables
+	public bool ShootFromCenter;
+	public GameObject Reticle;
+
+	// private variables
+	private Ray ray;
+	private RaycastHit hit;
+	private Vector3 ShootPosition;
+	
+	// Update is called once per frame
+	void Update () {
+		if(ShootFromCenter){
+			ShootPosition = 
+		}
+	}
+}
