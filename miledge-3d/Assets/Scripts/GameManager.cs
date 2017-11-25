ecb03b4d4f2b9fff54d70fdbfe154f95a9ac2c8d
AkashGutha <akash.gutha@outlook.com>
2017-11-25 14:59:05 +0530

    remove start function

--- miledge-3d/Assets/Scripts/GameManager.cs
+++ miledge-3d/Assets/Scripts/GameManager.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-
-	}
-
 	// Update is called once per frame
 	void Update () {
 		if (Application.platform == RuntimePlatform.Android) {
